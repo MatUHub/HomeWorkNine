8f9734e586c01a8b1d9a9117d6cc47a6d05933ff
Andej Nesterenko <mentatus.n@gmail.com>
2021-08-22 23:42:42 +0300

    Navigation.java

--- app/src/main/java/com/example/homeworknine/MainActivity.java
+++ app/src/main/java/com/example/homeworknine/MainActivity.java
@@ -8,17 +8,23 @@ import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Navigation navigation;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initToolbar();
-
+        navigation = new Navigation(getSupportFragmentManager());
         if(savedInstanceState == null){
-            getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, FragmentNote.newInstance()).commit();
+            //getSupportFragmentManager().beginTransaction().replace(R.id.container_fragment, FragmentNote.newInstance()).commit();
+            navigation.addFragment(FragmentNote.newInstance(),false);
         }
     }
 
+    public Navigation getNavigation() {
+        return navigation;
+    }
 
     private Toolbar initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
